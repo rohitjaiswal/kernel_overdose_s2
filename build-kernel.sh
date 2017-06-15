042338c9ad2ab44ed57f01d6e3d77d6aafba7a91
DarknessKiller <kaelvinchen2@gmail.com>
2017-06-15 15:35:51 +0800

    Update Build Script!
    Now Using CAF Toolchain & Make The Script Lighter

--- build-kernel.sh
+++ build-kernel.sh
@@ -1,14 +1,12 @@
     #!/bin/bash
-    echo Welcome To Automated Kernel Compiler For Le2
+    echo Overdose Kernel Compile Tool v2.0
     echo By DarknessKiller
+    echo Please Sit Back And Chill...
     ccache -c
     make clean && make mrproper
     echo Cleaned Some Old Crappy Files
-    export CROSS_COMPILE=/home/ubuntu/gcc-linaro-4.9.4-2017.01-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
-    echo Compiler Set Sucessful    
+    export CROSS_COMPILE=/home/ubuntu/aarch64-linux-android-4.9/bin/aarch64-linux-android-    
     export ARCH=arm64
-    echo Compiler Set To Build For ARM64
     make lineage_s2_defconfig
-    echo Picked Overdose Config
     make -j16
-    echo Building Has Been Completed
+    echo Kernel Has Been Compiled...
