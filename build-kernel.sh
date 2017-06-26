1715cdc7f8541e3dd394e3dfed90c90f6c5c58db
DarknessKiller <kaelvinchen2@gmail.com>
2017-06-26 06:52:31 +0000

    Build Script Update v2.0 With Rishabh Rao Commit
    Signed-off-by: admin@rishabhrao.net

--- build-kernel.sh
+++ build-kernel.sh
@@ -5,8 +5,8 @@
     ccache -c
     make clean && make mrproper
     echo Cleaned Some Old Crappy Files
-    export CROSS_COMPILE=/home/ubuntu/aarch64-linux-android-4.9/bin/aarch64-linux-android-    
+    export CROSS_COMPILE=/home/$USER/aarch64-linux-android-4.9/bin/aarch64-linux-android-    
     export ARCH=arm64
     make lineage_s2_defconfig
-    make -j16
+    make -j20
     echo Kernel Has Been Compiled...
