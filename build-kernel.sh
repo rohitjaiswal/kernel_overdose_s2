0602d54f827a589747808b49b51daff255952283
DarknessKiller <kaelvinchen2@gmail.com>
2017-06-28 14:40:30 +0000

    Update Build Script

--- build-kernel.sh
+++ build-kernel.sh
@@ -1,12 +1,22 @@
     #!/bin/bash
-    echo Overdose Kernel Compile Tool v2.0
+    setterm -clear
+    echo Overdose Kernel Compile Tool v3.0
     echo By DarknessKiller
     echo Please Sit Back And Chill...
     ccache -c
-    make clean && make mrproper
+    make mrproper
     echo Cleaned Some Old Crappy Files
+    export KBUILD_BUILD_USER="C4lvin"
+    export KBUILD_BUILD_HOST="DarknessCloud"
     export CROSS_COMPILE=/home/$USER/aarch64-linux-android-4.9/bin/aarch64-linux-android-    
     export ARCH=arm64
     make lineage_s2_defconfig
-    make -j20
-    echo Kernel Has Been Compiled...
+    make -j30 | tee /home/$USER/kernel_output.log
+    f="/home/$USER/kernel_overdose_s2/arch/arm64/boot/Image.gz-dtb"
+    i="0"
+    if [ -e $f ]
+    then
+    echo "Compile Success!"
+    else
+    echo "Compile Failed! Fix Your Error"
+    fi
